action masking, skip "no choice cells", fixed global obs for apex

models/common/models.py
@@ -4,9 +4,10 @@ import tensorflow as tf
 
 
 class FullyConnected(tf.Module):
-    def __init__(self, layers: List[int] = None, activation=tf.tanh, layer_norm=False, activation_out=True, name=None):
+    def __init__(self, layers: List[int] = None, activation=tf.tanh, layer_norm=False, activation_out=True,
+                 name="fully_connected_net"):
         super().__init__(name)
-        self.layers = []
+        self.layers = [tf.keras.layers.Flatten()]
         with self.name_scope:
             for i, num_hidden in enumerate(layers):
                 self.layers.append(tf.keras.layers.Dense(units=num_hidden))
@@ -24,7 +25,7 @@ class FullyConnected(tf.Module):
 
 class NatureCNN(tf.Module):
 
-    def __init__(self, activation_out=True, name=None):
+    def __init__(self, activation_out=True, name="nature_cnn_net"):
         super().__init__(name)
         with self.name_scope:
             self.layers = [
@@ -55,7 +56,7 @@ class ImpalaResidualLayer(NamedTuple):
 
 class ImpalaCNN(tf.Module):
 
-    def __init__(self, residual_layers: Iterable[Tuple[int, int]] = None, activation_out=True, name=None):
+    def __init__(self, residual_layers: Iterable[Tuple[int, int]] = None, activation_out=True, name="impala_cnn_net"):
         super().__init__(name)
         residual_layers = residual_layers or [(16, 2), (32, 2), (32, 2)]
         self._residual_layers = []

models/global_obs_model.py
@@ -1,4 +1,5 @@
 import gym
+import numpy as np
 import tensorflow as tf
 from flatland.core.grid import grid4
 from ray.rllib.models.tf.tf_modelv2 import TFModelV2
@@ -9,14 +10,43 @@ from models.common.models import NatureCNN, ImpalaCNN
 class GlobalObsModel(TFModelV2):
     def __init__(self, obs_space, action_space, num_outputs, model_config, name):
         super().__init__(obs_space, action_space, num_outputs, model_config, name)
+        assert isinstance(action_space, gym.spaces.Discrete), \
+            "Currently, only 'gym.spaces.Discrete' action spaces are supported."
+        self._action_space = action_space
         self._options = model_config['custom_options']
-        self._model = GlobalObsModule(action_space=action_space, architecture=self._options['architecture'],
-                                      name="global_obs_model", **self._options['architecture_options'])
+        self._mask_unavailable_actions = self._options.get("mask_unavailable_actions", False)
+
+        if self._mask_unavailable_actions:
+            obs_space = obs_space.original_space['obs']
+        else:
+            obs_space = obs_space.original_space
+
+        observations = [tf.keras.layers.Input(shape=o.shape) for o in obs_space]
+        processed_observations = preprocess_obs(tuple(observations))
+
+        if self._options['architecture'] == 'nature':
+            conv_out = NatureCNN(activation_out=True, **self._options['architecture_options'])(processed_observations)
+        elif self._options['architecture'] == 'impala':
+            conv_out = ImpalaCNN(activation_out=True, **self._options['architecture_options'])(processed_observations)
+        else:
+            raise ValueError(f"Invalid architecture: {self._options['architecture']}.")
+        logits = tf.keras.layers.Dense(units=action_space.n)(conv_out)
+        baseline = tf.keras.layers.Dense(units=1)(conv_out)
+        self._model = tf.keras.Model(inputs=observations, outputs=[logits, baseline])
+        self.register_variables(self._model.variables)
+        self._model.summary()
 
     def forward(self, input_dict, state, seq_lens):
-        obs = preprocess_obs(input_dict['obs'])
+        # obs = preprocess_obs(input_dict['obs'])
+        if self._mask_unavailable_actions:
+            obs = input_dict['obs']['obs']
+        else:
+            obs = input_dict['obs']
         logits, baseline = self._model(obs)
         self.baseline = tf.reshape(baseline, [-1])
+        if self._mask_unavailable_actions:
+            inf_mask = tf.maximum(tf.math.log(input_dict['obs']['available_actions']), tf.float32.min)
+            logits = logits + inf_mask
         return logits, state
 
     def variables(self):
@@ -42,25 +72,3 @@ def preprocess_obs(obs) -> tf.Tensor:
 
     return tf.concat(
         [tf.cast(transition_map, tf.float32), tf.cast(targets, tf.float32)] + processed_agents_state_layers, axis=-1)
-
-
-class GlobalObsModule(tf.Module):
-    def __init__(self, action_space, architecture: str, name=None, **kwargs):
-        super().__init__(name=name)
-        assert isinstance(action_space, gym.spaces.Discrete), \
-            "Currently, only 'gym.spaces.Discrete' action spaces are supported."
-        with self.name_scope:
-            if architecture == 'nature':
-                self._cnn = NatureCNN(activation_out=True, **kwargs)
-            elif architecture == 'impala':
-                self._cnn = ImpalaCNN(activation_out=True, **kwargs)
-            else:
-                raise ValueError(f"Invalid architecture: {architecture}.")
-            self._logits_layer = tf.keras.layers.Dense(units=action_space.n)
-            self._baseline_layer = tf.keras.layers.Dense(units=1)
-
-    def __call__(self, spatial_obs, non_spatial_obs=None):
-        latent_repr = self._cnn(spatial_obs)
-        logits = self._logits_layer(latent_repr)
-        baseline = self._baseline_layer(latent_repr)
-        return logits, baseline